6d6619ac7e92a6fcbc42383e3fe6f9e0dd257e93
Burhan Boz <boz.burhan8@gmail.com>
2025-06-16 15:24:04 +0300

    Initial mapita

---
+++ src/main/java/com/mapita/mapita_backend/MapitaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mapita.mapita_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapitaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapitaBackendApplication.class, args);
+	}
+
+}
